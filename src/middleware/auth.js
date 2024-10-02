6e0663abc68e7a23f985ed6d02ff3cc5ca7c7f6d
Deshmukh Sagar <deshmukh.sagar@3i-infotech.com>
2024-10-02 16:40:28 +0530

    AUTH CONTROLLER - REPONSE UTIL HANDLED

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.schema");
+const {response} = require("../utils/responseUtil");
 const userAuth = async (req, res, next) => {
    try {
       const { token } = req.cookies;
   
       if (!token) {
-        throw new Error("Invalid token");
+        return response(res,400,null , 'Invalid token');
       }
     
       const decodedToken = jwt.decode(token, "secret@123");
       if (!decodedToken) {
-        throw new Error("Invalid token");
+        return response(res,400,null , 'Invalid token');
       }
       if (decodedToken.id) {
         const user = await userModel.findById(decodedToken.id);
@@ -19,10 +20,10 @@ const userAuth = async (req, res, next) => {
           req.user = user;
           next();
         } else {
-          throw new Error("User not found");
+          return response(res,400,null , 'User not found');
         }
       } else {
-        throw new Error("Invalid token");
+        return response(res,400,null , 'Invalid token');
       }
    } catch (error) {
       res.status(400).send(error.message)
